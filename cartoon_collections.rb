8bf05a41260602e386f74ce62fb4e3b4b67bebd9
connor worthen <connorw220@protonmail.com>
2019-11-09 22:43:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,10 +10,9 @@ end
 def long_planeteer_calls(array)
   array.each do |str|
     return true unless str.size <= 4
+  else
+    return false if str.size <= 4
   end
-
-  true
-end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
