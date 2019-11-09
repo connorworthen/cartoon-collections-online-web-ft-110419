dff5a6af1b4982b1e36bde5d4ba32fa1ec5430ef
connor worthen <connorw220@protonmail.com>
2019-11-09 22:12:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.collect {|x| x + "!"}.downcase
+  array.collect {|x|puts "#{x.downcase+1}"}
 end
 
 def long_planeteer_calls# code an argument here
