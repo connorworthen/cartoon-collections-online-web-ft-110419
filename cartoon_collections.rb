7f83d1614d7f94b16393377519936b6598c64b9b
connor worthen <connorw220@protonmail.com>
2019-11-09 22:30:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ def summon_captain_planet(array)
 end
 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.select {|a| a > 3 }
 end
 
 def find_the_cheese# code an argument here
